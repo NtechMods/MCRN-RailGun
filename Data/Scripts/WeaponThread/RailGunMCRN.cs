2c820e266265cc4c7da4c7774ed757c05ff5373b
Bryon <30876973+nukeguard@users.noreply.github.com>
2020-03-24 04:11:04 -0400

    Update RailGunMCRN.cs

--- Data/Scripts/WeaponThread/RailGunMCRN.cs
+++ Data/Scripts/WeaponThread/RailGunMCRN.cs
@@ -68,7 +68,7 @@ namespace WeaponThread
                     TurretAttached = true,
                     TurretController = true,
                     PrimaryTracking = true,
-                    LockOnFocus = true,
+                    LockOnFocus = false,
                 },
                 HardWare = new HardwareDef
                 {
