9394cb9b244e1306ec2a97ca08c8fd7ad0d608d4
Bryon <30876973+nukeguard@users.noreply.github.com>
2021-01-28 15:50:11 -0500

    Update RailGunMCRN.cs

--- Data/Scripts/WeaponThread/RailGunMCRN.cs
+++ Data/Scripts/WeaponThread/RailGunMCRN.cs
@@ -58,7 +58,7 @@ namespace WeaponThread
                 WeaponName = "MCRNRails", // name of weapon in terminal
                 DeviateShotAngle = 0f,
                 AimingTolerance = 90f, // 0 - 180 firing angle
-                AimLeadingPrediction = Accurate, // Off, Basic, Accurate, Advanced
+                AimLeadingPrediction = Advanced, // Off, Basic, Accurate, Advanced
                 DelayCeaseFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
                 AddToleranceToTracking = false,
 
